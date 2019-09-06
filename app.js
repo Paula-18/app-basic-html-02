81805c32119ddc0ec11ef4efbe95df3881fd99f5
Paula-18 <paunahappy@gmail.com>
2019-09-06 12:38:40 -0700

    Tambor

--- app.js
+++ app.js
@@ -4,3 +4,10 @@ const doge = document.getElementById('doge');
 doge.addEventListener('click', ()=>{
     console.log('Guau, guau.');
 });
+
+drumButtons.forEach(
+    button =>{
+        button.style.left = `${button.dataset.posx}px`; 
+        button.style.top = `${button.dataset.posy}px`;
+    } 
+);
